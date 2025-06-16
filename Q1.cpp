2e65a2ddb63e04d62adf80d1974efe85caa58ad4
PercyWANG <pin-xuan.wang@connect.polyu.hk>
2025-06-16 18:43:10 +0800

    Days of Month Check
    This is a C++ program to compute the number of days in a given month and year.
    Definition of a leap year:
    Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible
    by 100, but these centurial years are leap years if they are exactly divisible by 400. For example,
    the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are.

---
+++ Q1.cpp
@@ -0,0 +1,38 @@
+// GA2Q1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+bool Leapyear(int year) {
+	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+int daysM(int month, int year)
+{
+	switch (month) {
+	case 2:
+		return Leapyear(year) ? 29 : 28;
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		return 30;
+		break;
+	default:
+		return 31;
+	}
+}
+int main(){
+	int year, month;
+	cout << "Enter a year: ";
+	cin >> year;
+	cout << "Enter a month: ";
+	cin >> month;
+	int days = daysM(month, year);
+	cout << "days are: " << days << endl;
+	int daysY;
+	daysY = Leapyear ? 366 : 365;
+	cout << daysY;
+	return 0;
+
+}
+

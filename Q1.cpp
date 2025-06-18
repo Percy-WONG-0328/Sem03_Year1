4f8f3b509cc5d0284f6a71714cac80010d5ec317
PercyWANG <pin-xuan.wang@connect.polyu.hk>
2025-06-18 09:55:40 +0800

    sort() function
    without using any extra libraries

--- Q1.cpp
+++ Q1.cpp
@@ -1,38 +1,50 @@
-// GA2Q1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
-bool Leapyear(int year) {
-	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-}
-int daysM(int month, int year)
-{
-	switch (month) {
-	case 2:
-		return Leapyear(year) ? 29 : 28;
-	case 4:
-	case 6:
-	case 9:
-	case 11:
-		return 30;
-		break;
-	default:
-		return 31;
-	}
-}
-int main(){
-	int year, month;
-	cout << "Enter a year: ";
-	cin >> year;
-	cout << "Enter a month: ";
-	cin >> month;
-	int days = daysM(month, year);
-	cout << "days are: " << days << endl;
-	int daysY;
-	daysY = Leapyear(year) ? 366 : 365;
-	cout << daysY;
-	return 0;
 
+void bubbleSort(int array[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                // Swap
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }
 
+// Function to calculate the median
+double findMedian(int array[], int size) {
+    bubbleSort(array, size);
+    if (size % 2 == 0) {
+        // Even number of elements
+        return (array[size / 2 - 1] + array[size / 2]) / 2.0;
+    }
+    else {
+        // Odd number of elements
+        return array[size / 2];
+    }
+}
+
+int main() {
+    const int MAX_SIZE = 100;
+    int array[MAX_SIZE];
+    int count = 0;
+
+    cout << "Enter integers (type s to stop): ";
+    int input;
+    while (cin >> input && input != 's' && count < MAX_SIZE) {
+        array[count++] = input;
+    }
+
+    if (count == 0) {
+        cout << "No numbers entered.\n";
+    }
+    else {
+        double median = findMedian(array, count);
+        cout << "Median: " << median << endl;
+    }
+
+    return 0;
+}
